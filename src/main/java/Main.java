03f168f4a96ca5484920f93bb673f80203b5dd43
alex21072003 <alexkarpinskiy21@gmail.com>
2024-10-17 15:06:46 +0300

    message

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // some comment
+        System.out.println("Message");
+        int capuchinoCofeeRequired = 60; // in grams;
+        /*многострочный комментарий с какой-то информацией*/
+        /**
+         * This variant is also for
+         * multi-line comments
+         */
+        int id = 546;
+    }
+}
